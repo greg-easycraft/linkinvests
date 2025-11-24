de5c322daf2efefdfeaee7ab7f7b74da9df41475
Grégoire EasyCraft <gregoire@easycraft.dev>
2025-11-24 11:23:34 +0100

    chore: remove unused imports from page component

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
 // import { SignInForm } from "~/components/auth/SignInForm";
 import Image from "next/image";
-import { Button } from "~/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
